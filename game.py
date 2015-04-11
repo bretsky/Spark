f6030f5d5cdd64a744b4f1fb0c63806d47effaf9
bretsky <bretskyselby99@gmail.com>
2015-04-11 01:39:20 -0700

    Add legendary weapon generator to main game, add menu title and background, change icon

--- game.py
+++ game.py
@@ -4,6 +4,33 @@ from decimal import *
 
 getcontext().prec = 64
 
+def legendary_setup():
+	legendary_data_dict = {}
+	for s in classes:
+		weapon_dict = {}
+		weapons_file = open(r'legendary_weapons\legendary_%(weapon)s.txt' % {'weapon' : s.replace(' ','_')})
+		weapons_data_list = weapons_file.read().splitlines()
+		weapons_split = []
+		weapons = []
+		for x in weapons_data_list:
+			x = x.split('/')
+			weapon_data = x
+			for x in weapon_data:
+				weapons_split.append(x)
+		weapons_info = []
+		for x in weapons_split:
+			if (weapons_split.index(x) + 1) % 2 == 1:
+				weapons.append(x)
+			else:
+				weapons_info.append(x)
+		weapon_dict['weapons'] = weapons
+		weapon_dict['weapons_info'] = weapons_info
+		legendary_data_dict[s] = weapon_dict
+	title_file = open('newspaper_titles.txt')
+	title_list = title_file.read().splitlines()
+	legendary_data_dict['titles'] = title_list
+	return legendary_data_dict
+
 def rationalize(weight_list):
 	weight_sum = 0
 	weight_ratio_list = []
@@ -177,6 +204,42 @@ def random_weapon(weapon_type='rand', maxlevel=100):
 	else:	
 		return weapon_chooser(weapon_type)
 
+def random_legendary_weapon(weapon_type='rand'):
+
+	def weapon_chooser(weapon_class):		
+		class_dict = legendary_data_dict[weapon_class]
+		weapons = class_dict['weapons']
+		random_weapon = random.choice(weapons)
+		weapon_index = weapons.index(random_weapon)  
+		weapon_attributes = {}
+		random_weapon_info = class_dict['weapons_info'][weapon_index]
+		weapon_attributes['info'] = random_weapon_info.replace('%t', random.choice(legendary_data_dict['titles']))
+		weapon_attributes['weapon'] = random_weapon
+		weapon_attributes['type'] = 'legendary ' + weapon_class 
+		weapon_attributes['material'] = 'legendary'
+		weapon_attributes['material_stat'] = 64
+		weapon_attributes['class'] = weapon_class
+		weapon_attributes['skill'] = weapon_class
+		weapon_attributes['level'] = 120
+		weapon = weapon_attributes['weapon']
+		weapon = ' '.join(word[0].upper() + word[1:] for word in weapon.split())
+		weapon_type = weapon_attributes['type']
+		weapon_type = ' '.join(word[0].upper() + word[1:] for word in weapon_type.split())
+		weapon_skill = weapon_attributes['skill']
+		weapon_skill = ' '.join(word[0].upper() + word[1:] for word in weapon_skill.split())
+		weapon_name = (weapon + ' (' + weapon_type + ' - No Material Bonus' + ' - Requires Level ' + str(weapon_attributes['level']) + ' ' + weapon_skill + ' Skill' + ')'+ ' Info: ' + weapon_attributes['info'])
+		weapon_attributes['name'] = weapon_name
+		return weapon_attributes
+
+	def weapon_class_chooser():
+		classes = ['ranged','long bladed','shield','magic', 'blunt', 'concealed', 'gadget', 'book']
+		weapon_class = random.choice(classes)
+		return weapon_class
+
+	if weapon_type == 'rand':
+		return weapon_chooser(weapon_class_chooser())
+	else:	
+		return weapon_chooser(weapon_type)
 
 def get_first_weapon(player_class):
 	beginning_weapons = []
@@ -224,6 +287,7 @@ def material_lister():
 
 materials = material_lister()
 classes = ['ranged','long bladed','shield','magic', 'blunt', 'concealed', 'gadget', 'book']
+legendary_data_dict = legendary_setup()
 data_dict = setup()
 print(intro())
 x = 0
--- spark.py
+++ spark.py
@@ -1,28 +1,63 @@
 import pygame
+from pygame.locals import *
 pygame.init()
 
-GameGo = True
 spark_animation = []
 for i in range(96):
 	spark = pygame.image.load('intro\spark (%(number)i).gif' % {'number' : i+1})
 	spark_animation.append(spark)
-screen = pygame.display.set_mode((1280, 720))
+screen_x = 1280
+screen_y = 720
+icon = pygame.image.load('spark_icon.gif')
+pygame.display.set_icon(icon)
+screen = pygame.display.set_mode((screen_x, screen_y))
 pygame.display.set_caption("Spark")
 
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((0, 0, 0))
 framerate = pygame.time.Clock()
-
 index = 0
-while GameGo:
-	framerate.tick(24)
-	screen.blit(background, (0,0))
-	screen.blit(spark_animation[index], (384, 312))
-	index+= 1
-	if index == 96:
-		index = 0
-	pygame.display.update()
+
+def intro():
+	intro_go = True
+	for x in range (96):
+		if intro_go == True:
+			framerate.tick(24)
+			screen.blit(background, (0,0))
+			screen.blit(spark_animation[x], (center(512, 96)))
+			pygame.display.update()
+			intro_go = quit_check()
+		
+def menu():
+	menu_go = True
+	menu_background = pygame.image.load('menu_background.gif')
+	menu_background = menu_background.convert()
+	menu_title = pygame.image.load('spark_text.gif')
+	menu_title = menu_title.convert()
+	while menu_go == True:
+		framerate.tick(24)
+		screen.blit(background, (0,0))
+		screen.blit(menu_background, (center(menu_background.get_width(), menu_background.get_height())))
+		title_position = int((screen.get_width()-menu_title.get_width())/2)
+		screen.blit(menu_title, (title_position, 150))
+		pygame.display.update()
+		menu_go = quit_check()
+
+
+def center(x, y):
+	center_x = (screen.get_width() - x)/2
+	center_y = (screen.get_height() - y)/2
+	return (center_x, center_y)
+
+
+def quit_check():
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			GameGo = False
+			return False
+	else:
+		return True
+
+
+intro()
+menu()
