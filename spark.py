2e4b275cc0021a5aa30e7d1ed307f1a7b089661b
bretsky <bretskyselby99@gmail.com>
2015-04-14 01:07:33 -0700

    Add font, add buttons to menu, add theme music and intro sound effects

--- spark.py
+++ spark.py
@@ -12,6 +12,9 @@ icon = pygame.image.load('spark_icon.gif')
 pygame.display.set_icon(icon)
 screen = pygame.display.set_mode((screen_x, screen_y))
 pygame.display.set_caption("Spark")
+SONG_END = pygame.USEREVENT + 1
+
+pygame.mixer.music.set_endevent(SONG_END)
 
 background = pygame.Surface(screen.get_size())
 background = background.convert()
@@ -21,34 +24,54 @@ index = 0
 
 def intro():
 	intro_go = True
+	pygame.mixer.music.load('sweep.mp3')
+	pygame.mixer.music.play(-1)
 	for x in range (96):
 		if intro_go == True:
 			framerate.tick(24)
 			screen.blit(background, (0,0))
-			screen.blit(spark_animation[x], (center(512, 96)))
+			screen.blit(spark_animation[x], (center(512,'x'), center (96, 'y')))
 			pygame.display.update()
 			intro_go = quit_check()
+	pygame.mixer.music.stop()
 		
 def menu():
 	menu_go = True
+	pygame.mixer.music.load('intro.mp3')
+	pygame.mixer.music.play(-1)
 	menu_background = pygame.image.load('menu_background.gif')
 	menu_background = menu_background.convert()
 	menu_title = pygame.image.load('spark_text.gif')
 	menu_title = menu_title.convert()
+	newgame_button = pygame.image.load('newgame_button.gif')
+	newgame_button = newgame_button.convert()
+	continue_button = pygame.image.load('continue_button.gif')
+	continue_button = continue_button.convert()
+	options_button = pygame.image.load('options_button.gif')
+	options_button = options_button.convert()
+	exit_button = pygame.image.load('exit_button.gif')
+	exit_button = exit_button.convert()
 	while menu_go == True:
 		framerate.tick(24)
 		screen.blit(background, (0,0))
-		screen.blit(menu_background, (center(menu_background.get_width(), menu_background.get_height())))
-		title_position = int((screen.get_width()-menu_title.get_width())/2)
-		screen.blit(menu_title, (title_position, 150))
+		screen.blit(menu_background, (center(menu_background.get_width(), 'x'), center(menu_background.get_height(), 'y')))
+		screen.blit(menu_title, (center(menu_title.get_width(), 'x'), (screen.get_height() - 280)/4 - menu_title.get_height()/2))
+		screen.blit(newgame_button, (center(newgame_button.get_width(),'x'), center(menu_background.get_height(), 'y')+15))
+		screen.blit(continue_button, (center(continue_button.get_width(),'x'), center(menu_background.get_height(), 'y')+79))
+		screen.blit(options_button, (center(options_button.get_width(),'x'), center(menu_background.get_height(), 'y')+143))
+		screen.blit(exit_button, (center(exit_button.get_width(),'x'), center(menu_background.get_height(), 'y')+207))
+		
 		pygame.display.update()
 		menu_go = quit_check()
+	pygame.mixer.music.stop()
 
-
-def center(x, y):
-	center_x = (screen.get_width() - x)/2
-	center_y = (screen.get_height() - y)/2
-	return (center_x, center_y)
+def center(x, dimension):
+	center_x = 0
+	if dimension == 'x':
+		center_x = (screen.get_width() - x)/2
+	if dimension == 'y':
+		center_x = (screen.get_height() - x)/2
+	return center_x
 
 
 def quit_check():
